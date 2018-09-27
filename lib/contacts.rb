461389391bd60b02da2ade8234b4ca045d5ce4b5
Rose Brennan <brennan.m.rose@gmail.com>
2018-09-27 11:50:50 +0000

    automatically backed up by learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -1,6 +1,3 @@
-require 'pry'
-
-def remove_strawberry(contacts)
 
   # contacts = {
   #   "Jon Snow" => {
@@ -15,12 +12,13 @@ def remove_strawberry(contacts)
   #   }
   # }
   
+  
+def remove_strawberry(contacts)
   contacts.each do |person, contact_details_hash|
     contact_details_hash.each do |attribute, data|
       if attribute == :favorite_ice_cream_flavors
         data.delete_if {|ice_cream| ice_cream == "strawberry"}
-      end 
+      end
     end
-  end 
-  
+  end
 end
